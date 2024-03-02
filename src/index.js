e7c269c3c965548b2767c16d8d98c1e81ea106ec
leonard <leonardocasa790@gmail.com>
2024-03-02 20:27:44 -0300

    chore: headers http

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import http from "node:http";
 
 const port = 3000;
 
+/*
+    Headers 
+    - "text/plain"
+    - "text/html"
+    - "application/xml"
+    - "multipart/form-data"
+    - "application/json"
+    - "image/jpeg, image/png, image/gif"
+*/
+
 const server  = http.createServer((req, res) => {
+
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("hello world");
+    res.setHeader("Content-Type", "application/json");
+    const responseData = { "say": "hello" };
+    const jsonResponse = JSON.stringify(responseData);
+    
+    res.end(jsonResponse);
 });
 
 server.listen(port, ()=> {
