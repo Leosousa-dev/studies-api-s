f074652f0796644031a3ca712589046ae535db47
leonard <leonardocasa790@gmail.com>
2024-03-04 23:29:04 -0300

    chore: create frist route

--- src/index.js
+++ src/index.js
@@ -13,14 +13,28 @@ const port = 3000;
 */
 
 const server  = http.createServer((req, res) => {
+    if(req.url === "/"){
+        res.statusCode = 200
+        res.setHeader("Content-Type", "text/html")
+        res.end("Hello")
+        return;
+    }
+    if(req.url === "/quotes" && req.method === "GET"){
+        const quote = {
+            quote: "The greatest of follies is sacrificing your health for any other type of happinessThe greatest of follies is sacrificing your health for any other type of happiness",
+            author: "Arthur Shopenhauer"
+        }
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    const responseData = { "say": "hello" };
-    const jsonResponse = JSON.stringify(responseData);
-    
-    res.end(jsonResponse);
-});
+        res.statusCode = 200
+        res.setHeader("Content-Type", "application/json")
+        res.end(JSON.stringify(quote))
+    }else{
+        res.statusCode = 400
+        res.setHeader("Content-Type", "text/html")
+        res.end("Not Found")
+        return;
+    }
+})
 
 server.listen(port, ()=> {
     console.log(`Server running at http://localhost:${port}/`);
